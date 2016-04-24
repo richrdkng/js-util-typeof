6ec388d4a14ce4dd508bb9c0c757c7193f644200
Richard King <richrdkng@gmail.com>
2016-04-24 16:58:13 +0200

    Add initial source file

---
+++ src/typeof.js
@@ -0,0 +1,113 @@
+/**
+ * typeof module.
+ *
+ * @module typeof
+ *
+ * @author Richard King <richrdkng@gmail.com> [GitHub]{@link https://github.com/richrdkng}
+ * @licence MIT
+ */
+
+/**
+ * UMD - returnExports.js pattern
+ * For more information and license, check the link below:
+ * [UMD GitHub Repository]{@link https://github.com/umdjs/umd}
+ */
+(function(root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = factory();
+    } else {
+        root.typeOf = factory();
+    }
+}(this, function () {
+    "use strict";
+
+    /** @var {undefined} */
+    var undefined = (function() {})();
+
+    /**
+     * Returns the type of the given object.
+     *
+     * By default the function will return similar results to the built-in "typeof" operator, but:
+     *    - when passing arrays it will return "array" as result
+     *    - when passing objects, it will return "object" as result
+     *
+     * @example
+     * // primitive types (undefined, null, Boolean, Number, String)
+     * typeOf(undefined) === "undefined";
+     * typeOf(null) === "null";
+     * typeOf(true) === "boolean";
+     * typeOf(0) === "number";
+     * typeOf("string") === "string";
+     *
+     * // reference types
+     * typeOf([]) === "array";
+     * typeOf({}) === "object";
+     * typeOf(function(){}) === "function";
+     * typeOf(new Date()) === "object";
+     * typeOf(new RegExp()) === "object";
+     * typeOf(new Error()) === "object";
+     *
+     * @arg {*}       object                  The object to return its type.
+     * @arg {boolean} specific       = false  Return the specific type of the given object.
+     * @arg {boolean} unmodifiedCase = false  Return the type as unmodified case.
+     *                                        By default the type will be lower-case.
+     *
+     * @returns {string} Type of the given object.
+     */
+    return function typeOf(object, specific, unmodifiedCase) {
+        var type,
+            str;
+
+        specific       = specific === true       ? specific       : false;
+        unmodifiedCase = unmodifiedCase === true ? unmodifiedCase : false;
+
+        if (object === undefined) {
+            type = "Undefined";
+
+        } else if (object === null) {
+            type = "Null";
+
+        } else {
+            switch (typeof object) {
+                case "boolean" :
+                    type = "Boolean";
+                    break;
+
+                case "number" :
+                    type = "Number";
+                    break;
+
+                case "string" :
+                    type = "String";
+                    break;
+
+                case "object" :
+                    if (!specific) {
+                        if(Object.prototype.toString.call(object) === "[object Array]") {
+                            type = "Array";
+
+                        } else {
+                            type = "Object";
+                        }
+
+                    } else {
+                        str  = Object.prototype.toString.call(object);
+                        type = str.substring(8, str.length-1);
+                    }
+                    break;
+
+                case "function" :
+                    type = "Function";
+                    break;
+            }
+        }
+
+        if (unmodifiedCase) {
+            return type;
+        } else {
+            return type.toLowerCase();
+        }
+    };
+}));
